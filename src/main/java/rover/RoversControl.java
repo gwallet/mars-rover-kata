39e59d11013cc304382c50236bea4377c23fde23
Guillaume Wallet <wallet.guillaume@gmail.com>
2017-02-10 17:56:33 +0100

    first solution

--- src/main/java/rover/RoversControl.java
+++ src/main/java/rover/RoversControl.java
@@ -1,10 +1,26 @@
 package rover;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import static kjss.lang.PreConditions.when;
+
 public class RoversControl {
+    private static final String OVER = "==========";
 
     public List<String> execute(List<String> script) {
-        return null;
+        when(script).isNull()
+                .throwIllegalArgument("expects script as list of strings, got '%s'", script);
+        Iterator<String> iterator = script.iterator();
+        iterator.next(); // skip plateau's dimension
+        List<String> output = new ArrayList<>();
+        while (iterator.hasNext()) {
+            Rover.at(Position.fromString(iterator.next()))
+                    .execute(Commands.fromString(iterator.next()))
+                    .logPosition(output);
+        }
+        output.add(OVER);
+        return output;
     }
 }
