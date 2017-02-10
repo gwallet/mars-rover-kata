ae92d5f4eaa69bc8ab2253aa708df205e31b31d4
Guillaume Wallet <wallet.guillaume@gmail.com>
2017-02-10 17:27:44 +0100

    Specification - Acceptance test

---
+++ src/main/java/rover/RoversControl.java
@@ -0,0 +1,10 @@
+package rover;
+
+import java.util.List;
+
+public class RoversControl {
+
+    public List<String> execute(List<String> script) {
+        return null;
+    }
+}
---
+++ src/test/java/rover/AcceptanceTest.java
@@ -0,0 +1,18 @@
+package rover;
+
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AcceptanceTest {
+    @Test
+    public void should_produce_expected_output() throws Exception {
+        List<String> input = Files.readAllLines(Paths.get("src", "test", "resources", "input.txt"));
+        List<String> output = Files.readAllLines(Paths.get("src", "test", "resources", "output.txt"));
+        assertThat(new RoversControl().execute(input)).isEqualTo(output);
+    }
+}
